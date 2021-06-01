51fff8708b3a87012efedc7add38eed2572e4cda
Ailton.S.Moreira <https://github.com/AiltonMoreira50/NosMorabeza.git>
2021-06-01 22:39:51 -0100

    NMorabeza

---
+++ app/src/main/java/com/example/nosmorabeza/Registo.kt
@@ -0,0 +1,13 @@
+package com.example.nosmorabeza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registo)
+    }
+
+
+}
