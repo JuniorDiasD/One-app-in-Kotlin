879f922a4b0b4d0449b306b638a9ee394a169ad2
Ailton.S.Moreira <https://github.com/AiltonMoreira50/NosMorabeza.git>
2021-06-01 23:52:52 -0100

    telaRegisto

--- app/src/main/java/com/example/nosmorabeza/Registo.kt
+++ app/src/main/java/com/example/nosmorabeza/Registo.kt
@@ -2,12 +2,26 @@ package com.example.nosmorabeza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_registo.*
 
 class Registo : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registo)
-    }
 
+        auth = FirebaseAuth.getInstance()
+    }
+private fun registro(){
+    registarBtn.setOnClickListener {
+
+       // auth.createUserWithEmailAndPassword()
+
+    }
+}
 
 }
